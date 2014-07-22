720492ae5414b4077ba36ac8e3ae40697da5e39e
Adam Grigolato <adam.grigolato@ezidebit.com.au>
2014-07-22 10:39:38 +1000

    Added testing and self execution to chatter

--- src/chatter.py
+++ src/chatter.py
@@ -16,13 +16,13 @@ import binascii
 MCAST_GRP = '224.1.1.192'
 MCAST_PORT = 8472
 
-def mcastsend(MCASTMSG):
+def mcastsend(MCAST_MSG):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-    sock.sendto(MCASTMSG, (MCAST_GRP, MCAST_PORT)
+    sock.sendto(MCAST_MSG, (MCAST_GRP, MCAST_PORT))
 
 
-def mcastrecv(MCASTMSG):
+def mcastrecv():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     try:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,28 +31,35 @@ def mcastrecv(MCASTMSG):
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
 
-    sock.bind((MCASTGRP,MCAST_PORT))
+    sock.bind((MCAST_GRP,MCAST_PORT))
     host = socket.gethostbyname(socket.gethostname())
     sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
-    sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCASTGRP) + socket.inet_aton(host))
+    sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_GRP) + socket.inet_aton(host))
 
-#while 1:
-#    try:
-#        data, addr = sock.recvfrom(1024)
-#    except socket.error, e:
-#        print 'Exception'
-#    hexdata = binascii.hexlify(data)
-#    print 'Data = %s' % hexdata
-
-
+    while 1:
+        try:
+            data, addr = sock.recvfrom(1024)
+        except socket.error, e:
+            print 'Exception'
+        hexdata = binascii.hexlify(data)
+        print 'Data = %s' % hexdata
 
 
 
 
 
 
-#if __name__ == '__main__':
-#    import argparse
-#
-#    main()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Send or recieve multicast test')
+    parser.add_argument('--receive', help='Receive Multicast tester',action="store_true")
+    parser.add_argument('--send', help='Send Multicast Tester')
+    args = parser.parse_args()
+    if args.receive:
+        mcastrecv()
+    elif args.send:
+        mcastsend(args.send)
 
